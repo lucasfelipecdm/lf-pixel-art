20543f7950a69b16f56f2c88657778a3d5b1f1cd
Lucas Felipe Costa da Mata <lucasfelipecdm@gmail.com>
2019-01-01 19:37:33 -0200

    feat: adds coments to JS file, udacity review

--- js/designs.js
+++ js/designs.js
@@ -1,15 +1,10 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-let gridHeight = 0;
-let gridWidth = 0;
-let color = null;
-
+/**
+ * This function creates the "<td>" and "<tr>" elements in HTML document, it get the size of grids from
+ * "<input type="number">" elements call inputHeight and inputWidth.
+ */
 function makeGrid() {
-    gridHeight = $('#inputHeight').val();
-    gridWidth = $('#inputWidth').val();
+    let gridHeight = $('#inputHeight').val();
+    let gridWidth = $('#inputWidth').val();
     for (let y = 0; y < gridHeight; y++){
         $('#pixelCanvas').append('<tr id="'+y+'"></tr>');
         for (let x = 0; x < gridWidth; x++){
@@ -17,18 +12,26 @@ function makeGrid() {
         }
     }
 }
+/**
+ * This function get the color from "<input type="color">" element and return it.
+ */
 
-function pickColor() {
-    color = $('#colorPicker').val();
+function pickColor() {    
+    return $('#colorPicker').val();
 }
 
+/**
+ * This function change the background-color of "<td>" when this element is clicked.
+ */
 function paintPixel(pixel){
     $(pixel).on('click', function(){
-        pickColor();
-        $(this).css('background-color', color);
+        $(this).css('background-color', pickColor());
     })
 }
 
+/**
+ * This function call the makeGrid funtion and creates a event listener for each "<td>" element in HTML document.
+ */
 $('#gridSubmit').on('click', function(){
     $('#pixelCanvas').html('');
     makeGrid();
