803541a62b345ffdabf462366b391f1fb9820101
Lucas Felipe Costa da Mata <lucasfelipecdm@gmail.com>
2019-01-01 10:22:51 -0200

    feat: Creates grid on pixel canvas

--- js/designs.js
+++ js/designs.js
@@ -3,8 +3,21 @@
 
 // When size is submitted by the user, call makeGrid()
 
+let gridHeight = 0;
+let gridWidth = 0;
+
 function makeGrid() {
-
-// Your code goes here!
-
+    gridHeight = $('#inputHeight').val();
+    gridWidth = $('#inputWidth').val();
+    for (let y = 0; y < gridHeight; y++){
+        $('#pixelCanvas').append('<tr id="'+y+'"></tr>');
+        for (let x = 0; x < gridWidth; x++){
+            $('#'+y).append('<td></td>');
+        }
+    }
 }
+
+$('#gridSubmit').on('click', function(){
+    $('#pixelCanvas').html('');
+    makeGrid();
+})
